cc3458ec3154b6dab16eae68b44905110ae18bed
M-lakshan <emlakshan92@gmail.com>
2025-08-05 00:38:39 +0530

    completed feed page

--- bigbytes/src/app/feed/page.tsx
+++ bigbytes/src/app/feed/page.tsx
@@ -3,8 +3,9 @@
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import type { RootState } from "@/redux/store/store";
-import { useRouter } from "next/navigation";
 import SinglePost from "@/components/specific/post";
+import Development from "@/app/under-dev";
+
 import clsx from "clsx";
 
 type FeedProps = {
@@ -18,17 +19,22 @@ export default function Feed({ classList }: FeedProps) {
   const cls = clsx(classList?.tw, classList?.cs);
   const user = useSelector((state: RootState) => state.user);
   const blog = useSelector((state: RootState) => state.blog);
+  const underDevelopment = useSelector((state: RootState) => state.development?.pages?.feed);
   const [pageLayout, setPageLayout] = useState("list");
-  
+    
+
+  if(underDevelopment) return <Development />
+
   return <main id="feed" className={cls}>
-    <h2>Feed Page</h2>
     {(blog?.posts.length >= 1 && blog?.publishers.length >= 1) ? <div className="blog_posts">
       {blog.posts.map((eachBP,idx) => (blog.publishers.map(pbl => pbl.id  ).includes(eachBP.publisher)) ? <SinglePost
-        key={`bp_${idx}_${eachBP.id}`}
+        key={`bp_${idx+1}_${eachBP.id}`}
         classList={{
           tw: [],
           cs: []
         }}
+        publisher_alt={blog?.publishers?.find(pb => pb.id === eachBP.publisher)}
+        coauthor_alt={blog?.coauthors?.find(ca => ca.id === eachBP.coauthor)}
         postObj={eachBP}
         type="feed"
         layout={pageLayout}
