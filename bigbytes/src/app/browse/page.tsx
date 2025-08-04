3ef8d38d2d8b9af0c8bd6f77242c4bdff77b1a57
M-lakshan <emlakshan92@gmail.com>
2025-08-05 00:35:08 +0530

    sample browse page

--- bigbytes/src/app/browse/page.tsx
+++ bigbytes/src/app/browse/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { useSelector } from "react-redux";
+import type { RootState } from "@/redux/store/store";
+import Development from "@/app/under-dev";
 import clsx from "clsx";
 
 type ResultProps = {
@@ -9,6 +14,9 @@ type ResultProps = {
 
 export default function Result({ classList }: ResultProps) {
   const cls = clsx(classList?.tw, classList?.cs);
+  const underDevelopment = useSelector((state: RootState) => state.development?.pages?.browse);
+    
+  if(underDevelopment) return <Development type={true}/>
 
   return <main id="Result" className={cls}>
     <h2>Result Page</h2>    
