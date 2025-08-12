a93cd2e1fce084d436bde93255b3947e3cd5f764
M-lakshan <emlakshan92@gmail.com>
2025-08-12 20:44:20 +0530

    forground background colours replaced

--- bigbytes/src/app/not-found.tsx
+++ bigbytes/src/app/not-found.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <main id="not_found" className="min-h-screen flex flex-col items-center justify-center p-8 text-center">
-      <h1 className="text-6xl font-bold mb-4">404</h1>
+      <h1 className="text-4xl md:text-6xl font-bold mb-4 text-pink-600">⚠️&nbsp;404 : {` NOT FOUND `}</h1>
       <p className="text-xl mb-8">Sorry, the page you are looking for was not found.</p>
       <Link
         href="/"
-        className="px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+        className="action_btn"
       >
-        Go back home
+        Home
       </Link>
     </main>
   );
