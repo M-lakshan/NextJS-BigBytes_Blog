8806f4fc675aa639224741e8346688e959172423
M-lakshan <emlakshan92@gmail.com>
2025-08-16 12:57:41 +0530

    browse > [slug] page updated

--- bigbytes/src/app/browse/[slug]/page.tsx
+++ bigbytes/src/app/browse/[slug]/page.tsx
@@ -1,3 +1,10 @@
+'use client';
+
+import { useSelector, useDispatch } from "react-redux";
+import type { RootState } from "@/redux/store/store";
+import { SinglePostAlt } from "@/components/specific/post";
+import { getCMSContentBySlug } from "@/lib/cms";
+import path from "path";
 import clsx from "clsx";
 
 type SpecifPostProps = {
@@ -5,12 +12,30 @@ type SpecifPostProps = {
     tw?: string[];
     cs?: string[];
   }
+  params: { slug: string[] }
 }
 
-export default function SpecifPost({ classList }: SpecifPostProps) {
+export default async function SpecifPost({ classList, params }: SpecifPostProps) {
   const cls = clsx(classList?.tw, classList?.cs);
+  const user = useSelector((state: RootState) => state.user);
+  const CMS_CONTENT_PATH = path.join(process.cwd(), "content/cms");
+  const slug = params.slug.join("/");
+  const secPhaseBlogPost = await getCMSContentBySlug(slug, CMS_CONTENT_PATH);
 
   return <main id="specific_post" className={cls}>
     <h2>Post Page</h2>    
+    
+    <SinglePostAlt
+      classList={{
+        tw: [],
+        cs: []
+      }}
+      publisher_alt={null}
+      coauthor_alt={null}
+      postObj={secPhaseBlogPost}
+      format="feed"
+      layout="grid"
+      session_exists={(user?.sts)}
+    />
   </main>
 }
