fd10ca23ff24c08f2d70c8847dcda5861502ed5a
M-lakshan <emlakshan92@gmail.com>
2025-08-17 19:50:11 +0530

    proper API mock sample added for LEADCMS data fetch

---
+++ bigbytes/src/app/api/cms/[slug]/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import path from "path";
+import { getCMSContentBySlug } from "@/lib/cms";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { slug?: string[] } }
+) {
+  if (!params.slug || !Array.isArray(params.slug)) {
+    console.error("Missing or invalid 'slug' in params:", params.slug);
+    return NextResponse.json({ error: "Missing slug" }, { status: 400 });
+  }
+
+  const CMS_CONTENT_PATH = path.join(process.cwd(), ".leadcms", "content");
+  // const CMS_CONTENT_PATH = path.join(process.cwd(), "content/cms");
+  const slug = params.slug.join("/");
+
+  console.log("##### "+CMS_CONTENT_PATH);
+
+  const data = await getCMSContentBySlug(slug, CMS_CONTENT_PATH);
+
+  if (data) {
+    return NextResponse.json(data);
+  } else {
+    return NextResponse.json({ error: "Not found" }, { status: 404 });
+  }
+}
