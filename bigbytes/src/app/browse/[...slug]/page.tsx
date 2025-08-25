3b841aac6766ebb186c946a6762d460a5e61757f
M-lakshan <emlakshan92@gmail.com>
2025-08-26 02:43:59 +0530

    refined to be directly fetch specific post content

--- bigbytes/src/app/browse/[...slug]/page.tsx
+++ bigbytes/src/app/browse/[...slug]/page.tsx
@@ -1,6 +1,29 @@
 import SpecificPost from "./specificPost";
+import PostTemplate from "@/components/specific/slugPostFix";
+import NotFound from "@/app/not-found";
+import { getCMSContentBySlug } from "@/lib/cms";
+import path from "path";
 
-export default function Page({ params }: { params: { slug: string[] } }) {
+const CMS_CONTENT_PATH = path.join(process.cwd(), ".leadcms", "content");
+
+// export default function Page({ params }: { params: { slug: string[] } }) {
   
-  return <SpecificPost slugs={params} />;
+//   return <SpecificPost slugs={params} />;
+// }
+
+interface PageProps {
+  params: { slug: string[] }
+}
+
+export default async function Page({ params }: PageProps) {
+  const slug = params.slug.join("/")
+  const content = await getCMSContentBySlug(slug, CMS_CONTENT_PATH)
+  
+  // if (loading) return <ProgressState />; // wont be needing - direct building contents
+  if (!content) return <NotFound 
+    type="post"
+    redirect="browse"
+  />
+
+  return <PostTemplate content={content} />
 }
